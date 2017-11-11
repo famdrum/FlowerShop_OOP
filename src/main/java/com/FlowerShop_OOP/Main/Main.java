26ba416ecf1dd3f600200d31fc41a1c20a6c42b7
Taras Mazurkevych <mad.taras@gmail.com>
2017-11-11 15:29:29 +0200

    Remove redundant code

--- src/main/java/com/FlowerShop_OOP/Main/Main.java
+++ src/main/java/com/FlowerShop_OOP/Main/Main.java
@@ -1,24 +1,8 @@
 package com.FlowerShop_OOP.Main;
 
-
-
-import com.FlowerShop_OOP.Entity.Shop;
-
-import java.util.Date;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Shop shop = new Shop();
-        shop.fillShopWithGoods();
-        System.out.println(shop);
-
-
-        
-
-
-
 
     }
 }
