73cd63fa3e6184232b2d99fdc14cb5ff00e6d9c5
VictorYezhov <vicyezh@gmail.com>
2017-11-11 13:45:57 +0200

    Add Maven support

--- src/com/FlowerShop_OOP/Specification/NarcissusSpecification.java
+++
@@ -1,9 +0,0 @@
-package com.FlowerShop_OOP.Specification;
-
-import java.util.Date;
-
-public class NarcissusSpecification extends Specification {
-    NarcissusSpecification(int price, String name, String smell, int lengthOfStem, Date freshness, String originCountry) {
-        super(price,  smell, lengthOfStem, freshness, originCountry);
-    }
-}
--- src/com/FlowerShop_OOP/Specification/TulipSpecification.java
+++
@@ -1,9 +0,0 @@
-package com.FlowerShop_OOP.Specification;
-
-import java.util.Date;
-
-public class TulipSpecification extends Specification {
-    TulipSpecification(int price, String name, String smell, int lengthOfStem, Date freshness, String originCountry) {
-        super(price,  smell, lengthOfStem, freshness, originCountry);
-    }
-}
