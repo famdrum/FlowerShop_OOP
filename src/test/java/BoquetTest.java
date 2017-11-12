74cddad2aa9d01e0b48d542d0b36256cf0acf58e
Taras Mazurkevych <mad.taras@gmail.com>
2017-11-12 22:57:44 +0200

    Add tests

---
+++ src/test/java/BoquetTest.java
@@ -0,0 +1,40 @@
+package com.FlowerShop_OOP.Entity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BoquetTest {
+    @Test
+    public void getPlants() throws Exception {
+    }
+
+    @Test
+    public void setPlants() throws Exception {
+    }
+
+    @Test
+    public void getPrice() throws Exception {
+    }
+
+    @Test
+    public void setPrice() throws Exception {
+    }
+
+    @Test
+    public void getAvarageLenght() throws Exception {
+    }
+
+    @Test
+    public void setAvarageLenght() throws Exception {
+    }
+
+    @Test
+    public void addFlower() throws Exception {
+    }
+
+    @Test
+    public void removeFlower() throws Exception {
+    }
+
+}
