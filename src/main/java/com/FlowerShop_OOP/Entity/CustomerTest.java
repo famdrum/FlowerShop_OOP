e64cfaef25c509ed66e3666ada980b6b69ed98e6
Taras Mazurkevych <mad.taras@gmail.com>
2017-11-12 19:08:01 +0200

    Add tests

---
+++ src/main/java/com/FlowerShop_OOP/Entity/CustomerTest.java
@@ -0,0 +1,25 @@
+package com.FlowerShop_OOP.Entity;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+    @Test
+    public void charge() throws Exception {
+        Customer customer = new Customer(200);
+        customer.charge(20);
+        assertThat("Wrong balance", 180, is(customer.getBalance()));
+    }
+
+    @Test
+    public void setMoneyAvailable() throws Exception {
+    }
+
+    @Test
+    public void getBalance() throws Exception {
+    }
+
+}
